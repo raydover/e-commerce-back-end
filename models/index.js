e218a96587d97a5a3013c784043e06d2d7b5d6d1
raydover <105178034+raydover@users.noreply.github.com>
2022-07-04 15:17:57 -0400

    create data model section for ProductTag.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tag'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_product'
+});
+
 
 module.exports = {
   Product,
